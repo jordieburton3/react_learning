3f92f91e33fa26febd6f2e892245f1b255ecfd8a
jmb145 <jmb145@duke.edu>
2017-11-20 18:16:02 -0500

    added more classes to the playground class file

--- react-course_projects/indecision_app/src/playground/es6-classes-1.js
+++ react-course_projects/indecision_app/src/playground/es6-classes-1.js
@@ -9,16 +9,54 @@ class Person {
     }
 
     getDescription() {
-        return `${this.name} is ${this.age} years old`
+        return `${this.name} is ${this.age} years old.`
     }
 }
 
-const me = new Person("Jordan Burton", 21);
-console.log(me.getGreeting());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description +=  ` Their major is ${this.major}`
+        }
+        return description;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, home) {
+        super(name, age);
+        this.home = home;
+    }
+
+    getGreeting() {
+        let greeting = `Hi, I am ${this.name}!`;
+        if (this.home) {
+            greeting += ` I am visiting from ${this.home}`
+        }
+        return greeting;
+    }
+}
+
+const me = new Traveler("Jordan Burton", 21, "Warwick, New York");
+console.log(me);
 
 console.log(me.getDescription());
 
-const other = new Person();
+console.log(me.getGreeting());
+
+const other = new Traveler();
 console.log(other);
 
 console.log(other.getDescription());
+
+console.log(other.getGreeting());
