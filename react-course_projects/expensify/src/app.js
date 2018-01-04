3c07068bce9cb10d51bab39cc4b20ca24260517a
Jordan Burton <jordieburton3@gmail.com>
2018-01-04 00:00:57 -0500

    Removed dummy data

--- react-course_projects/expensify/src/app.js
+++ react-course_projects/expensify/src/app.js
@@ -10,27 +10,6 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 const store = configureStore();
-
-store.dispatch(addExpense({ 
-    description: "Water Bill",
-    note: "",
-    amount: 4500,
-}));
-
-store.dispatch(addExpense({ 
-    description: "Gas Bill",
-    note: "",
-    amount: 300,
-    createdAt: 1000
-}));
-
-store.dispatch(addExpense({ 
-    description: "Rent",
-    note: "",
-    amount: 109500,
-}));
-
-console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
  
 const jsx = (
     <Provider store={store}>
